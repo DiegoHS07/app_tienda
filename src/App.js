399179a9fd7c4f41270697d2c3f71bdc85b524f9
DiegoHS07 <dhiguera09@gmail.com>
2023-03-02 11:36:40 -0500

    Creación de productos

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ import Tienda from './components/Tienda';
 import Error404 from './components/Error404';
 
 const App = () => {
+  const productos = [
+    {id: 1, nombre: 'Producto 1'},
+    {id: 2, nombre: 'Producto 2'},
+    {id: 3, nombre: 'Producto 3'},
+    {id: 4, nombre: 'Producto 4'},
+  ];
+
   return (
     <Contenedor>
       <Menu>
@@ -19,7 +26,10 @@ const App = () => {
           <Route path='*' element={<Error404/>}/>
           <Route path='/' element={<Inicio/>}/>
           <Route path='/blog' element={<Blog/>}/>
-          <Route path='/tienda' element={<Tienda/>}/>
+          <Route path='/tienda' element={
+              <Tienda productos={productos} />
+            }
+          />
         </Routes>
       </main>
       <aside>
--- src/components/Tienda.js
+++ src/components/Tienda.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import Productos from './Productos';
 
-const Tienda = () => {
+const Tienda = ({productos}) => {
     return (
         <div>
             <h1>Tienda</h1>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis id et, nisi, autem deleniti, possimus pariatur quasi cumque minus accusamus distinctio alias adipisci itaque aperiam inventore fuga quaerat illo dolorum.
-            </p>
+            <Productos productos={productos}/>
         </div>
     );
 }
