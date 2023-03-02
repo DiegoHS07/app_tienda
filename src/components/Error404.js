c553cbfdf87185694668e57a1546afcb1d3752fa
DiegoHS07 <dhiguera09@gmail.com>
2023-03-02 11:16:25 -0500

    Creación de páginas y rutas

---
+++ src/components/Error404.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Error404 = () => {
+    return (
+        <div>
+            <h1>Error 404</h1>
+            <p>
+                La página no existe o fue movida de lugar.
+            </p>
+        </div>
+    );
+}
+ 
+export default Error404;
---
+++ src/components/Inicio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Inicio = () => {
+    return (
+        <div>
+            <h1>Inicio</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis id et, nisi, autem deleniti, possimus pariatur quasi cumque minus accusamus distinctio alias adipisci itaque aperiam inventore fuga quaerat illo dolorum.
+            </p>
+        </div>
+    );
+}
+ 
+export default Inicio;
